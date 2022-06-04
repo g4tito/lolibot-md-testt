d7ad28e9e9bef5e41520a85c194ada17e638b196
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-04 13:07:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
+  ['51940617554', '👑 Mystic - Creador 👑', true],
   ['50575008120', '🤖 Mystic - Bot 🤖', true],
   ['17722386341']
-] // Cambia los numero que quieras
+]
 global.mods = [] 
 global.prems = [] 
 global.APIs = { // API Prefix
