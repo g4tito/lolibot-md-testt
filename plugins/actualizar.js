170053423c9c2a348d9862416d5806ff58937ee6
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-04 18:20:27 -0500

    Update and rename owner-update.js to actualizar.js

--- plugins/actualizar.js
+++ plugins/actualizar.js
@@ -1,11 +1,15 @@
 import { execSync } from 'child_process'
+
 let handler = async (m, { conn, text }) => {
 if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)
-}}
-handler.help = ['update']
+ }
+}
+
+handler.help = ['actualizar']
 handler.tags = ['owner']
-handler.command = /^update|actualizar$/i
-handler.rowner = true
+handler.command = /^(update|fix|actualizar)$/i
+handler.owner = true
+
 export default handler
