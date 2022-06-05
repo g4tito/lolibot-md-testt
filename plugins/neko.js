a6dcf6a0c4ccb101a5402ecf06569eaf8fe22d6c
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-05 14:17:26 -0500

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, command }) => {
 let ne = await (await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')).text()
 let nek = ne.split('\n')
 let neko = pickRandom(nek)
-conn.sendFile(m.chat, neko, 'error.png', 'NEKO 🐾💗', m)
+conn.sendFile(m.chat, neko, 'error.png', '*NEKO 🐾💗*', m)
 }
 
 handler.help = ['neko']
