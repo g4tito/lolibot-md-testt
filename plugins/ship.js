50911776c8f2a4db368a83f9d4e45a1a580ddd27
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-04 21:07:12 -0500

    Update ship.js

--- plugins/ship.js
+++ plugins/ship.js
@@ -1,43 +1,17 @@
-let R = Math.random
-let Fl = Math.floor
-let toM = a => '@' + a.split('@')[0]
+let user = a => '@' + a.split('@')[0]
 
 let handler = async (m, { conn, command, text, groupMetadata }) => {
-    let ps = groupMetadata.participants.map(v => v.jid)
-    let a = ps[Fl(R() * ps.length)]
-    let b
-    do b = ps[Fl(R() * ps.length)]
-    while (b === a)
-    let c
-    do c = ps[Fl(R() * ps.length)]
-    while (b === a)
-    let d
-    do d = ps[Fl(R() * ps.length)]
-    while (b === a)
-    let e
-    do e = ps[Fl(R() * ps.length)]
-    while (b === a)
-    let f
-    do f = ps[Fl(R() * ps.length)]
-    while (b === a)
-    let g
-    do g = ps[Fl(R() * ps.length)]
-    while (b === a)
-    let h
-    do h = ps[Fl(R() * ps.length)]
-    while (b === a)
-    let i
-    do i = ps[Fl(R() * ps.length)]
-    while (b === a)
-    let j
-    do j = ps[Fl(R() * ps.length)]
-    while (b === a)
 
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b = ps.getRandom()
+
+let ps = groupMetadata.participants.map(v => v.id)let a = ps.getRandom()let b = ps.getRandom()
 let crz = (pickRandom(['💖', '❤', '💞', '💓', '💗', '❣', '💟']))
 
 let ship = `\t\t*‧ ${crz} La pareja del dia ${crz} ‧*
 
-*${toM(a)} + ${toM(b)}*`
+*${user(a)} + ${user(b)}*`
 m.reply(ship)
 
 }
