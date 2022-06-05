2f1681de039342d3f09eca3146023e410ad76f63
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-05 10:18:09 -0500

    Update google.js

--- plugins/google.js
+++ plugins/google.js
@@ -1,4 +1,4 @@
-import { googleIt } from '@bochilteam/scraper'
+/*import { googleIt } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, usedPrefix, command, args }) => {
 const fetch = (await import('node-fetch')).default
@@ -26,4 +26,32 @@ handler.help = ['google']
 handler.tags = ['internet']
 handler.command = /^(google)$/i
 
+export default handler*/
+
+
+import MessageType from '@adiwajshing/baileys'
+import fetch from 'node-fetch'
+import googleIt from 'google-it'
+
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+  let full = /goo$/i.test(command)
+  let text = args.join` `
+  if (!text) return conn.reply(m.chat, `*Ingrese un texto que desea buscar*\n\n- Ejemplo: ${usedPrefix + command} Minecraft`, m)
+  conn.reply(m.chat, wait, m)
+  let url = 'https://google.com/search?q=' + encodeURIComponent(text)
+  let search = await googleIt({ query: text })
+  let msg = search.map(({ title, link, snippet}) => {
+    return `*${title}*\n_${link}_\n_${snippet}_`
+  }).join`\n\n─────────────────\n\n`
+  let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
+  if (ss.includes('html')) throw ''
+  await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
+}
+
+handler.help = ['google']
+handler.tags = ['internet']
+handler.command = /^(google|googlef)$/i
+
 export default handler
+
+
