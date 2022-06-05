c7fe5ce5c4cdcd5c50c6104905faa07aac8395b6
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-04 19:18:32 -0500

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -1,4 +1,4 @@
-import os from 'os'
+/*import os from 'os'
 import util from 'util'
 import { sizeFormatter } from 'human-readable'
 import speed from 'performance-now'
@@ -82,15 +82,16 @@ function clockString(ms) {
   let s = Math.floor(ms / 1000) % 60
   console.log({ms,h,m,s})
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
+}*/
 
-/*
+
 import os from 'os'
 import util from 'util'
 import sizeFormatter from 'human-readable'
 import MessageType from '@adiwajshing/baileys'
 import fs from 'fs'
 import { performance } from 'perf_hooks'
+
 let handler = async (m, { conn, usedPrefix }) => {
 let _uptime = process.uptime() * 1000
 let uptime = clockString(_uptime) 
@@ -98,37 +99,25 @@ let totalreg = Object.keys(global.db.data.users).length
 const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
 const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
 const groups = chats.filter(([id]) => id.endsWith('@g.us'))
+const totalfeatures = Object.values(global.plugins).filter( (v) => v.help && v.tags ).length
 const used = process.memoryUsage()
 const { restrict } = global.db.data.settings[conn.user.jid] || {}
 const { autoread } = global.opts
-let old = performance.now()
-let neww = performance.now()
-let speed = neww - old
+const timestamp = speed()
+const latensi = speed() - timestamp
+const ownum = "51940617554@s.whatsapp.net"
 let info = `
-╠═〘 𝐈𝐍𝐅𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓 〙 ═
-╠
-╠➥ [🤴🏻] 𝙲𝚁𝙴𝙰𝙳𝙾𝚁: *𝙱𝚛𝚞𝚗𝚘 𝚂𝚘𝚋𝚛𝚒𝚗𝚘*
-╠➥ [#️⃣] 𝙽𝙾° 𝙳𝙴𝙻 𝙲𝚁𝙴𝙰𝙳𝙾𝚁: *+52 1 999 209 5479*
-╠➥ [🎳] 𝙿𝚁𝙴𝙵𝙸𝙹𝙾: *${usedPrefix}*
-╠➥ [🔐] 𝙲𝙷𝙰𝚃𝚂 𝙿𝚁𝙸𝚅𝙰𝙳𝙾𝚂: *${chats.length - groups.length}*
-╠➥ [🦜] 𝙲𝙷𝙰𝚃𝚂 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂: *${groups.length}* 
-╠➥ [💡] 𝙲𝙷𝙰𝚃𝚂 𝚃𝙾𝚃𝙰𝙻𝙴𝚂: *${chats.length}* 
-╠➥ [🚀] 𝙰𝙲𝚃𝙸𝚅𝙸𝙳𝙰𝙳: *${uptime}*
-╠➥ [🎩] 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂: *${totalreg} 𝚗𝚞𝚖𝚎𝚛𝚘𝚜*
-╠➥ [👨‍🦯] 𝚅𝙴𝙻𝙾𝙲𝙸𝙳𝙰𝙳: 
-╠  *${speed}* 
-╠  *𝚖𝚒𝚕𝚒𝚜𝚎𝚐𝚞𝚗𝚍𝚘𝚜*
-╠➥ [☑️] 𝙰𝚄𝚃𝙾𝚁𝙴𝙰𝙳: ${autoread ? '*𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘*' : '*𝚍𝚎𝚜𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘*'}
-╠➥ [❗] 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝚃: ${restrict ? '*𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘*' : '*𝚍𝚎𝚜𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘*'} 
-╠
-╠═〘 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭 〙 ═
+🐋〃 Creador: @${ownum.split("@s.whatsapp.net")[0]}
+❄️〃 Comandos: ${totalfeatures} Total
+🐋〃 Prefijo: 「 *${usedPrefix}* 」
+❄️〃 Velocidad: ${latensi.toFixed(4)} Segundos
+🐋〃 Chat Privado: ${chats.length - groups.length}
+❄️〃 Chat de Grupos: ${groups.length}
+🐋〃 Chat Totales: ${chats.length}
+❄️〃 Tiempo activo: ${uptime
+🐋〃 Usuarios: ${totalreg} Numeros
 `.trim() 
-conn.reply(m.chat, info, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙸𝙽𝙵𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃',
-body: 'ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})
+conn.reply(m.chat, info, text, { quoted: m, contextInfo: { externalAdReply:{title: `↷✦╎Info - Bot╎💌˖ ⸙`, previewType:"PHOTO",thumbnail: false, sourceUrl:``}, mentionedJid: [ownum]}})
 }
 handler.help = ['infobot', 'speed']
 handler.tags = ['info', 'tools']
@@ -141,4 +130,4 @@ let m = Math.floor(ms / 60000) % 60
 let s = Math.floor(ms / 1000) % 60
 console.log({ms,h,m,s})
 return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
-*/
+
