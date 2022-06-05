661b67772b287009a66e0cd58098de2e1a2095a7
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-04 20:18:57 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -137,7 +137,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       //totalexp: exp,
       //xp4levelup: max - exp,
       github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
-      level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
+      //level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
