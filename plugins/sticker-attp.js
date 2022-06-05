77d48c0666647515769138224fe4f6f0106e6e7e
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-05 13:44:20 -0500

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,9 +1,12 @@
 import { sticker } from '../lib/sticker.js'
+
 let handler = async (m, { conn, text }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
 conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
-handler.help = ['attp <teks>']
+
+handler.help = ['attp']
 handler.tags = ['sticker']
-handler.command = /^attp$/i
+handler.command = /^(attp)$/i
+
 export default handler
