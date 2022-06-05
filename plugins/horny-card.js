20b40c31557dd4360666e15599e523e0561c49fe
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-05 14:08:51 -0500

    Update horny-card.js

--- plugins/horny-card.js
+++ plugins/horny-card.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/horny', {
 avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-}), 'error.png', '*HORNYCARD 🥵🔥*', m)
+}), 'error.png', '*HORNY CARD 🥵🔥*', m)
 }
 
 handler.help = ['hornycard']
