7f65b2448e73f8bfce089cc27e1228ebbe95fc0a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-05 09:27:05 -0500

    Update convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,11 +1,11 @@
 import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-const notStickerMessage = `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙸𝙼𝙰𝙶𝙴𝙽 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`
-if (!m.quoted) throw notStickerMessage
+let isStik = 'Etiqueta un sticker con el comando ${usedPrefix}toimg'
+if (!m.quoted) throw isStik
 const q = m.quoted || m
 let mime = q.mediaType || ''
-if (!/sticker/.test(mime)) throw notStickerMessage
+if (!/sticker/.test(mime)) throw isStik
 let media = await q.download()
 let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
 await conn.sendFile(m.chat, out, 'error.png', '*Se convirtió a imágen*', m)
