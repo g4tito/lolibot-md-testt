faeed7c763d0944311cb9a23a177a1a3af85351a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-04 22:12:53 -0500

    Update registrar.js

--- plugins/registrar.js
+++ plugins/registrar.js
@@ -7,7 +7,7 @@ let Reg = /(.*)([.|])([0-9]*)$/i
 let Rname = /([A-Za-z])$/i
 
 let handler = async function (m, { conn, text, usedPrefix, command}) {
-  let user = global.DATABASE._data.users[m.sender]
+  let user = db.data.users[m.sender]
   if (user.registered === true) throw `*Ya estas registrado*, quieres volver a registrarte?\nUse ${usedPrefix}unreg su codigo`
   if (!Reg.test(text)) throw `*Registro no valido*\n- Ejemplo: ${usedPrefix + command} nombre|edad\n\nNo se olvide del "|"`
   let [_, name, splitter, age] = text.match(Reg)
