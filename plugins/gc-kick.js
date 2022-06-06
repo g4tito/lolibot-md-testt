7dcbc21336dee617d5c8703135b53cd3564e87d7
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-05 19:17:12 -0500

    Update gc-kick.js

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -26,7 +26,7 @@ let handler = async (m, { command, text, groupMetadata, participants }) => {
   let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
   //if (!m.chat) return m.reply('Etiqueta a alguien del grupo para eliminar!')
   //if (!groupMetadata.participants.some(v => v.jid === user)) 
-  if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) return m.reply('El usuario ya no está en el grupo!')
+  if (!user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) return m.reply('El usuario ya no está en el grupo!')
   let owr = m.chat.split`-`[0]
   if (user.startsWith(owr)) return m.reply('No puedo eliminarlo\'a por que el creó el grupo')
   await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
